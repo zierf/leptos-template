cec63a8aac19065276125b09ed6a196ffbb4ed38
Florian Zier <9168602+zierf@users.noreply.github.com>
2024-10-15 01:07:18 +0200

    add persistence to global state

--- src/app.rs
+++ src/app.rs
@@ -14,17 +14,48 @@ struct GreetArgs<'a> {
     name: &'a str,
 }
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
 struct GlobalState {
     count: i32,
-    name: String
+    name: String,
 }
 
 #[component]
 pub fn App() -> impl IntoView {
-    provide_context(create_rw_signal(GlobalState::default()));
+    // provide global state with persistence in localStorage
+    let local_storage = web_sys::window()
+        .expect("globalThis object")
+        .local_storage()
+        .expect("local storage property")
+        .expect("storage instance");
+
+    let storage_value = local_storage
+        .get_item("global-state")
+        .expect("optional storage item");
+
+    logging::log!("localStorage = {}", format!("{:?}", storage_value));
+
+    provide_context(create_rw_signal(match storage_value {
+        None => GlobalState::default(),
+        Some(value_string) => serde_json::from_str(&value_string).expect("global-state"),
+    }));
 
     let state = expect_context::<RwSignal<GlobalState>>();
+
+    // update changes to global state in localStorage
+    create_effect(move |_| {
+        logging::log!(
+            "StateChange = {}",
+            state.with(|state| format!("{:?}", state))
+        );
+
+        let serialized =
+            serde_json::to_string(&state.with(|state| state.clone())).expect("serialized value");
+
+        local_storage
+            .set_item("global-state", &serialized)
+            .expect("setting an item");
+    });
 
     view! {
         <main class="container">
@@ -89,9 +120,15 @@ pub fn Greeter() -> impl IntoView {
                 return;
             }
 
-            let args = serde_wasm_bindgen::to_value(&GreetArgs { name: &name }).unwrap();
+            let args = serde_wasm_bindgen::to_value(&GreetArgs { name: &name })
+                .expect("greeting arguments");
+
             // Learn more about Tauri commands at https://v2.tauri.app/develop/calling-rust/
-            let new_msg = invoke("greet", args).await.as_string().unwrap();
+            let new_msg = invoke("greet", args)
+                .await
+                .as_string()
+                .expect("returned message string");
+
             set_greet_msg(new_msg);
         });
     };
