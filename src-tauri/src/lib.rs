27dbf7876ff550746cb396f2a9fb8d459d6eee63
Florian Zier <9168602+zierf@users.noreply.github.com>
2024-12-01 17:59:25 +0100

    open DevTools for debug builds

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::Manager;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,6 +10,17 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            let main_window = app
+                .get_webview_window("main")
+                .ok_or("Could not find main window!")?;
+
+            // open DevTools for Debug Builds
+            #[cfg(debug_assertions)]
+            main_window.open_devtools();
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
